91aeeeeedf39dca79bd28fd98b6c895f9d5b0309
DaveRoox <davexrusso@gmail.com>
2019-12-09 07:01:29 +0100

    day9 2019

--- 2019/day9.py
+++ 2019/day9.py
@@ -1,13 +1,19 @@
+def index(program, p, x, rel_base):
+    v = 10 ** (x + 2)
+    mode = (program[p] % v) // (v / 10)
+    index = program[p + x] if mode == 0 else (p + x) if mode == 1 else (program[p + x] + rel_base)
+    while len(program) <= index:
+        program.append(0)
+    return index
+
+
 def run(program, inputs):
     p, i, rel_base = 0, 0, 0
     while p < len(program):
         op_code = program[p] % 100
         if op_code == 99:  # exit
             return
-        first_mode = (program[p] % 1000) // 100
-        first_index = program[p + 1] if first_mode == 0 else (p + 1) if first_mode == 1 else (program[p + 1] + rel_base)
-        while len(program) <= first_index:
-            program.append(0)
+        first_index = index(program, p, 1, rel_base)
         first_operand = program[first_index]
         if op_code == 3:  # input
             program[first_index] = inputs[i]
@@ -20,16 +26,10 @@ def run(program, inputs):
             rel_base += first_operand
             p += 2
         else:
-            second_mode = (program[p] % 10000) // 1000
-            second_index = program[p + 2] if second_mode == 0 else (p + 2) if second_mode == 1 else (program[p + 2] + rel_base)
-            while len(program) <= second_index:
-                program.append(0)
+            second_index = index(program, p, 2, rel_base)
             second_operand = program[second_index]
             if op_code in (1, 2, 7, 8):
-                third_mode = (program[p] % 100000) // 10000
-                third_index = program[p + 3] if third_mode == 0 else (p + 3) if third_mode == 1 else (program[p + 3] + rel_base)
-                while len(program) <= third_index:
-                    program.append(0)
+                third_index = index(program, p, 3, rel_base)
                 if op_code == 1:  # add
                     program[third_index] = first_operand + second_operand
                 elif op_code == 2:  # multiply

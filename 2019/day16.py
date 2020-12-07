341d0ad8ad4cc737f851c91ed2e61a72749e2e63
DaveRoox <davexrusso@gmail.com>
2020-12-08 00:04:48 +0100

    day 16

--- 2019/day16.py
+++ 2019/day16.py
@@ -1,22 +1,30 @@
 from functools import reduce
 
 
-def prod(inp, patt, phase):
-    return abs(reduce(lambda acc, i: acc + inp[i] * patt[((i + 1) // phase) % len(patt)], range(len(inp)), 0)) % 10
+def part1(input_signal):
+    def apply_phase(signal):
+        out_signal, s = [], len(signal)
+        for pos in range(1, s + 1):
+            out_signal.append(abs(
+                sum(sum(signal[i:i + pos]) for i in range(pos - 1, s, 4 * pos)) -
+                sum(sum(signal[i:i + pos]) for i in range(pos - 1 + 2 * pos, s, 4 * pos))
+            ) % 10)
+        return out_signal
+
+    print(*reduce(lambda s, _: apply_phase(s), range(100), input_signal)[:8], sep='')
 
 
-def generate(digits, times):
-    for _ in range(times):
-        digits = list(map(lambda phase: prod(digits, [0, 1, 0, -1], phase + 1), range(len(digits))))
-    return digits
-
-
-with open('./day16.txt') as f:
-    digits = list(map(int, list(f.readline())))
-    print(*generate(digits[:], times=100)[:8], sep='')  # part 1
-    offset = int(''.join(map(str, digits[:7])))
-    digits *= 10000
+def part2(signal):
+    offset = int(''.join(map(str, signal[:7])))
+    signal *= 10000
     for _ in range(100):
-        for i in range(len(digits) - 2, offset - 1, -1):
-            digits[i] = (digits[i] + digits[i + 1]) % 10
-    print(*digits[offset:offset + 8], sep='')  # part 2
+        for i in range(len(signal) - 2, offset - 1, -1):
+            signal[i] = (signal[i] + signal[i + 1]) % 10
+
+    print(*signal[offset:offset + 8], sep='')
+
+
+with open("day16.txt") as f:
+    m = list(map(int, f.readline()))
+    part1(m)
+    part2(m)

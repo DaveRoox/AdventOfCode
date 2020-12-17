5e2ef56eb723e8b124aa907bed0777411ee4130d
DaveRoox <davexrusso@gmail.com>
2020-12-17 15:04:46 +0100

    day 17 (improved)

--- 2020/day17.py
+++ 2020/day17.py
@@ -18,40 +18,44 @@ def count_active_4d(active, x, y, z, w):
                if (x + dx, y + dy, z + dz, w + dw) in active)
 
 
-def check_inactive_neighbors_3d(active, x, y, z):
-    return set((x + dx, y + dy, z + dz)
-               for dx in (-1, 0, 1)
-               for dy in (-1, 0, 1)
-               for dz in (-1, 0, 1)
-               if (x + dx, y + dy, z + dz) not in active
-               and count_active_3d(active, x + dx, y + dy, z + dz) == 3)
-
-
-def check_inactive_neighbors_4d(active, x, y, z, w):
-    return set((x + dx, y + dy, z + dz, w + dw)
-               for dx in (-1, 0, 1)
-               for dy in (-1, 0, 1)
-               for dz in (-1, 0, 1)
-               for dw in (-1, 0, 1)
-               if (x + dx, y + dy, z + dz, w + dw) not in active
-               and count_active_4d(active, x + dx, y + dy, z + dz, w + dw) == 3)
-
-
-def step(active, count_active, check_inactive_neighbors):
+def step_3d(active):
     s = set()
-    for coords in active:
-        if count_active(active, *coords) in (3, 4):
-            s.add(coords)
-        s = s.union(check_inactive_neighbors(active, *coords))
+    min_x, max_x = min(p[0] for p in active), max(p[0] for p in active)
+    min_y, max_y = min(p[1] for p in active), max(p[1] for p in active)
+    min_z, max_z = min(p[2] for p in active), max(p[2] for p in active)
+    for x in range(min_x - 1, max_x + 2):
+        for y in range(min_y - 1, max_y + 2):
+            for z in range(min_z - 1, max_z + 2):
+                pos = (x, y, z)
+                active_neigh = count_active_3d(active, *pos)
+                if pos in active and active_neigh in (3, 4) or pos not in active and active_neigh == 3:
+                    s.add(pos)
+    return s
+
+
+def step_4d(active):
+    s = set()
+    min_x, max_x = min(p[0] for p in active), max(p[0] for p in active)
+    min_y, max_y = min(p[1] for p in active), max(p[1] for p in active)
+    min_z, max_z = min(p[2] for p in active), max(p[2] for p in active)
+    min_w, max_w = min(p[3] for p in active), max(p[3] for p in active)
+    for x in range(min_x - 1, max_x + 2):
+        for y in range(min_y - 1, max_y + 2):
+            for z in range(min_z - 1, max_z + 2):
+                for w in range(min_w - 1, max_w + 2):
+                    pos = (x, y, z, w)
+                    active_neigh = count_active_4d(active, *pos)
+                    if pos in active and active_neigh in (3, 4) or pos not in active and active_neigh == 3:
+                        s.add(pos)
     return s
 
 
 def part1(g):
-    print(len(reduce(lambda acc, _: step(acc, count_active_3d, check_inactive_neighbors_3d), range(6), g)))
+    print(len(reduce(lambda acc, _: step_3d(acc), range(6), g)))
 
 
 def part2(g):
-    print(len(reduce(lambda acc, _: step(acc, count_active_4d, check_inactive_neighbors_4d), range(6), g)))
+    print(len(reduce(lambda acc, _: step_4d(acc), range(6), g)))
 
 
 with open("input/day17.txt") as f:

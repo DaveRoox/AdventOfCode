691e3579f9e1c05826d446ed7743099ca7794908
DaveRoox <davexrusso@gmail.com>
2020-12-22 16:44:28 +0100

    day 22

--- 2020/day22.py
+++ 2020/day22.py
@@ -16,19 +16,18 @@ def part1(deck1, deck2):
 
 def part2(deck1, deck2):
     def play_game(d1, d2):  # returns True if player1 wins the game
-        d1_states, d2_states = set(), set()
+        state = set()
         while len(d1) > 0 and len(d2) > 0:
 
-            if (len(d1), d1[0], d1[-1]) in d1_states and (len(d2), d2[0], d2[-1]) in d2_states:
+            t = (len(d1), d1[0], d1[-1], len(d2), d2[0], d2[-1])
+            if t in state:
                 return True
             else:
-                d1_states.add((len(d1), d1[0], d1[-1]))
-                d2_states.add((len(d2), d2[0], d2[-1]))
+                state.add(t)
 
             c1, c2 = d1.pop(0), d2.pop(0)
             if len(d1) >= c1 and len(d2) >= c2:
-                sub_d1, sub_d2 = d1[:c1], d2[:c2]
-                p1_wins = max(sub_d1) >= max(sub_d2) or play_game(sub_d1, sub_d2)
+                p1_wins = play_game(d1[:c1], d2[:c2])
             else:
                 p1_wins = c1 >= c2
             if p1_wins:
@@ -37,6 +36,7 @@ def part2(deck1, deck2):
             else:
                 d2.append(c2)
                 d2.append(c1)
+
         return len(d1) > 0
 
     play_game(deck1, deck2)

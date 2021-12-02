0cde670133c288fcd7b1a8c680669820203f9b1c
DaveRoox <davexrusso@gmail.com>
2021-12-02 17:12:52 +0100

    go refactoring

--- 2021/day02.go
+++ 2021/day02.go
@@ -3,72 +3,46 @@ package main
 import (
 	"aoc/utils"
 	"fmt"
-	"strconv"
 	"strings"
 )
 
-type command struct {
-	Cmd string
-	N   int64
-}
-
-func toCommands(ls []string) (cmds []command) {
-	for _, s := range ls {
-		cmds = append(cmds, func(s string) command {
-			c := strings.Split(s, " ")
-			n, _ := strconv.ParseInt(c[1], 10, 64)
-			return command{
-				Cmd: c[0],
-				N:   n,
-			}
-		}(s))
-	}
-	return
-}
-
 // Part1 of day2
-func Part1(cmds []command) {
+func Part1(cmds []string) {
 	depth, hor := 0, 0
-	m := map[string]func(int){
-		"up": func(n int) {
-			depth -= n
-		},
-		"down": func(n int) {
-			depth += n
-		},
-		"forward": func(n int) {
-			hor += n
-		},
-	}
 	for _, cmd := range cmds {
-		m[cmd.Cmd](int(cmd.N))
+		splCmd := strings.Split(cmd, " ")
+		switch n := utils.ToIntOrDie(splCmd[1]); splCmd[0] {
+		case "up":
+			depth -= n
+		case "down":
+			depth += n
+		case "forward":
+			hor += n
+		}
 	}
 	fmt.Println(depth * hor)
 }
 
 // Part2 of day2
-func Part2(cmds []command) {
+func Part2(cmds []string) {
 	aim, depth, hor := 0, 0, 0
-	m := map[string]func(int){
-		"up": func(n int) {
+	for _, cmd := range cmds {
+		splCmd := strings.Split(cmd, " ")
+		switch n := utils.ToIntOrDie(splCmd[1]); splCmd[0] {
+		case "up":
 			aim -= n
-		},
-		"down": func(n int) {
+		case "down":
 			aim += n
-		},
-		"forward": func(n int) {
+		case "forward":
 			hor += n
 			depth += aim * n
-		},
-	}
-	for _, cmd := range cmds {
-		m[cmd.Cmd](int(cmd.N))
+		}
 	}
 	fmt.Println(depth * hor)
 }
 
 func main() {
-	cmds := toCommands(utils.ReadFileLinesOrDie("./input/day02.txt"))
+	cmds := utils.ReadFileLinesOrDie("./input/day02.txt")
 	Part1(cmds)
 	Part2(cmds)
 }

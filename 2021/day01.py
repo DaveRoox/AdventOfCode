ef0e05ed62d3f24923dc3e2aa88e48c0ffe4bb64
DaveRoox <davexrusso@gmail.com>
2021-12-02 16:39:34 +0100

    day 1

--- 2021/day01.py
+++ 2021/day01.py
@@ -1,9 +1,9 @@
 def part1(nums):
-    print(sum(map(lambda t: int.__lt__(*t), zip(nums, nums[1:]))))
+    print(sum(x < y for x, y in zip(nums, nums[1:])))
 
 
 def part2(nums):
-    print(sum(map(lambda t: int.__lt__(*t), zip(nums, nums[3:]))))
+    print(sum(x < y for x, y in zip(nums, nums[3:])))
 
 
 with open("input/day01.txt") as f:

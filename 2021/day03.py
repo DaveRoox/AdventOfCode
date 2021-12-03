4fc86daf38f32c16d6b7f0dee1d4b94b2eb49c2b
DaveRoox <davexrusso@gmail.com>
2021-12-03 08:32:15 +0100

    day 03

---
+++ 2021/day03.py
@@ -0,0 +1,31 @@
+from functools import reduce
+
+
+def most_common_bit(nums, pos):
+    return int(sum(num[pos] == '1' for num in nums) >= len(nums) / 2)
+
+
+def least_common_bit(nums, pos):
+    return 1 - most_common_bit(nums, pos)
+
+
+def part1(nums):
+    gamma_rate = reduce(lambda acc, pos: (acc << 1) | most_common_bit(nums, pos), range(len(nums[0])))
+    print(gamma_rate * (~gamma_rate & ((1 << len(nums[0])) - 1)))
+
+
+def part2(nums):
+    def filter_down(arr, strategy):
+        pos = 0
+        while len(arr) > 1:
+            arr = list(filter(lambda n: n[pos] == str(strategy(arr, pos)), arr))
+            pos += 1
+        return int(arr[0], 2)
+
+    print(filter_down(nums, most_common_bit) * filter_down(nums, least_common_bit))
+
+
+with open("input/day03.txt") as f:
+    v = list(map(lambda l: l.replace('\n', ''), f.readlines()))
+    part1(v)
+    part2(v)

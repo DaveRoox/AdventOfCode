f12a71dfd29ecfd148f981c76f4b99a83f76c481
DaveRoox <davexrusso@gmail.com>
2021-12-10 08:58:09 +0100

    day 10

--- 2021/day10.py
+++ 2021/day10.py
@@ -1,12 +1,13 @@
 from functools import reduce
+from statistics import median
 
 
-def diagnose(line):
+def diagnose(v):
     stack, closing_map = [], {'{': '}', '(': ')', '<': '>', '[': ']'}
-    for c in line:
+    for c in v:
         if c in closing_map:
             stack.append(c)
-        elif len(stack) == 0 or c != closing_map[stack[-1]]:
+        elif not stack or c != closing_map[stack[-1]]:
             return True, c  # True if corrupted
         else:
             stack.pop()
@@ -14,18 +15,20 @@ def diagnose(line):
 
 
 def part1(v):
-    print(sum(map(lambda p: {')': 3, ']': 57, '}': 1197, '>': 25137}[p[1]] if p[0] else 0, map(diagnose, v))))
+    print(sum(map(lambda c: {')': 3, ']': 57, '}': 1197, '>': 25137}[c], v)))
 
 
 def part2(v):
-    def get_score(l):
-        return reduce(lambda score, c: 5 * score + {')': 1, ']': 2, '}': 3, '>': 4}[c], l, 0)
-
-    scores = sorted(map(get_score, map(lambda p: p[1], filter(lambda p: not p[0], map(diagnose, v)))))
-    print(scores[len(scores) // 2])
+    print(median(map(lambda l: reduce(lambda score, c: 5 * score + {')': 1, ']': 2, '}': 3, '>': 4}[c], l, 0), v)))
 
 
 with open("input/day10.txt") as f:
-    v = [line.replace('\n', '') for line in f.readlines()]
-    part1(v)
-    part2(v)
+    corrupted, incomplete = [], []
+    for l in map(diagnose, (line.replace('\n', '') for line in f.readlines())):
+        is_corrupted, faulty = l
+        if is_corrupted:
+            corrupted.append(faulty)
+        else:
+            incomplete.append(faulty)
+    part1(corrupted)
+    part2(incomplete)

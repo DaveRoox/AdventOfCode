3ee6f657b91da70a2a55a70acbbaecb047e79930
DaveRoox <davexrusso@gmail.com>
2021-12-13 09:38:16 +0100

    day 13

--- 2021/day13.py
+++ 2021/day13.py
@@ -2,13 +2,8 @@ from functools import reduce
 
 
 def fold(points, instruction):
-    def max_x():
-        return max(points, key=lambda p: p[0])[0]
-
-    def max_y():
-        return max(points, key=lambda p: p[1])[1]
-
-    x_line, y_line = (int(instruction[1]), max_y()) if instruction[0] == 'x' else (max_x(), int(instruction[1]))
+    x_line, y_line = (int(instruction[1]), float('inf')) if instruction[0] == 'x' else (
+        float('inf'), int(instruction[1]))
     return set((x if x <= x_line else 2 * x_line - x, y if y <= y_line else 2 * y_line - y) for x, y in points)
 
 
@@ -18,8 +13,8 @@ def part1(points, folds):
 
 def part2(points, folds):
     def print_points(points):
-        min_x, max_x = min(points, key=lambda p: p[0])[0], max(points, key=lambda p: p[0])[0]
-        min_y, max_y = min(points, key=lambda p: p[1])[1], max(points, key=lambda p: p[1])[1]
+        min_x, max_x = min(p[0] for p in points), max(p[0] for p in points)
+        min_y, max_y = min(p[1] for p in points), max(p[1] for p in points)
         for yy in range(min_y, 1 + max_y):
             for xx in range(min_x, 1 + max_x):
                 print('#' if (xx, yy) in points else ' ', end='')

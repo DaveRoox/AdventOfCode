96c1925dcdf88ad4d4976b945ab20bc72d386aa8
DaveRoox <davexrusso@gmail.com>
2021-12-15 12:48:20 +0100

    day 15

--- 2021/day15.py
+++ 2021/day15.py
@@ -1,11 +1,10 @@
 from queue import PriorityQueue
 
 
-def dijkstra(g):
-    d, curr_cell = {(i, j): float('inf') for i in range(len(g)) for j in range(len(g[0]))}, (0, 0)
-    d[curr_cell] = 0
-    pq, visited = PriorityQueue(), set()
-    pq.put((0, curr_cell))
+def dijkstra(rows, cols, value_for):
+    distances, visited = {(0, 0): 0}, set()
+    pq = PriorityQueue()
+    pq.put((0, (0, 0)))
 
     while not pq.empty():
         _, curr_cell = pq.get()
@@ -14,27 +13,30 @@ def dijkstra(g):
         for di, dj in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
             ni, nj = i + di, j + dj
             new_cell = (ni, nj)
-            if 0 <= ni < len(g) and 0 <= nj < len(g[0]) and new_cell not in visited:
-                old_cost, new_cost = d[new_cell], d[curr_cell] + g[ni][nj]
+            if 0 <= ni < rows and 0 <= nj < cols and new_cell not in visited:
+                old_cost, new_cost = distances.get(new_cell, float('inf')), distances[curr_cell] + value_for(ni, nj)
                 if new_cost < old_cost:
                     pq.put((new_cost, new_cell))
-                    d[new_cell] = new_cost
+                    distances[new_cell] = new_cost
 
-    return d[(len(g) - 1, len(g[0]) - 1)]
+    return distances[(rows - 1, cols - 1)]
 
 
 def part1(grid):
-    print(dijkstra(grid))
+    print(dijkstra(
+        rows=len(grid),
+        cols=len(grid[0]),
+        value_for=lambda i, j: grid[i][j]))
 
 
 def part2(grid):
-    def new_value(i, j):
-        def modulo(x):
-            return x if x <= 9 else x - 9
+    def modulo(x):
+        return x if x <= 9 else x - 9
 
-        return modulo(grid[i % len(grid)][j % len(grid[0])] + i // len(grid) + j // len(grid[0]))
-
-    print(dijkstra([[new_value(i, j) for j in range(5 * len(grid[0]))] for i in range(5 * len(grid))]))
+    print(dijkstra(
+        rows=5 * len(grid),
+        cols=5 * len(grid[0]),
+        value_for=lambda i, j: modulo(grid[i % len(grid)][j % len(grid[0])] + i // len(grid) + j // len(grid[0]))))
 
 
 with open("input/day15.txt") as f:

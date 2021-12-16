23b02f5eb8ad1dfb2ada31d8787f6820537b32bf
DaveRoox <davexrusso@gmail.com>
2021-12-16 14:00:20 +0100

    day 16

--- 2021/day16.py
+++ 2021/day16.py
@@ -1,69 +1,63 @@
 from functools import reduce
 
 
-def wrap_execution(bits, vn_f=None):
+def wrap_execution(bits):
+    def extract_int(nbits):
+        nonlocal idx
+        idx += nbits
+        return reduce(lambda acc, n: acc << 1 | n, bits[idx - nbits:idx])
 
-    def extract_int(bit_length):
-        nonlocal bits, i
-        i += bit_length
-        return reduce(lambda acc, n: acc << 1 | n, bits[i - bit_length:i])
+    def extract_bit():
+        return extract_int(nbits=1)
 
     def parse_number():
-        nonlocal bits, i
         number, can_continue = 0, True
         while can_continue:
-            can_continue = extract_int(bit_length=1)
-            number = number << 4 | extract_int(bit_length=4)
+            can_continue = extract_bit()
+            number = number << 4 | extract_int(nbits=4)
         return number
 
     def parse_packet():
-        nonlocal bits, i, vn_f
-        packet_version, type_id = extract_int(bit_length=3), extract_int(bit_length=3)
-        if vn_f:
-            vn_f(packet_version)
+        nonlocal idx, version_numbers
+        packet_version, type_id = extract_int(nbits=3), extract_int(nbits=3)
+        version_numbers += packet_version
 
         if type_id == 4:
             return parse_number()
-
-        numbers, length_type_id = [], extract_int(bit_length=1)
-        if length_type_id == 0:
-            total_length = extract_int(bit_length=15)
-            total_length += i
-            while i < total_length:
-                numbers.append(parse_packet())
         else:
-            numbers = [parse_packet() for _ in range(extract_int(bit_length=11))]
+            numbers = []
+            if extract_bit() == 0:
+                total_length = extract_int(nbits=15)
+                total_length += idx
+                while idx < total_length:
+                    numbers.append(parse_packet())
+            else:
+                numbers = [parse_packet() for _ in range(extract_int(nbits=11))]
 
-        if type_id == 0:
-            return sum(numbers)
-        if type_id == 1:
-            return reduce(int.__mul__, numbers, 1)
-        if type_id == 2:
-            return min(numbers)
-        if type_id == 3:
-            return max(numbers)
-        if type_id == 5:
-            return 1 if numbers[0] > numbers[1] else 0
-        if type_id == 6:
-            return 1 if numbers[0] < numbers[1] else 0
-        return 1 if numbers[0] == numbers[1] else 0
+            if type_id == 0:
+                return sum(numbers)
+            if type_id == 1:
+                return reduce(int.__mul__, numbers, 1)
+            if type_id == 2:
+                return min(numbers)
+            if type_id == 3:
+                return max(numbers)
+            if type_id == 5:
+                return 1 if numbers[0] > numbers[1] else 0
+            if type_id == 6:
+                return 1 if numbers[0] < numbers[1] else 0
+            return 1 if numbers[0] == numbers[1] else 0
 
-    i = 0
-    return parse_packet()
+    idx, version_numbers = 0, 0
+    return parse_packet(), version_numbers
 
 
 def part1(bits):
-    def add_to_version_number(vn):
-        nonlocal version_numbers
-        version_numbers += vn
-
-    version_numbers = 0
-    wrap_execution(bits, vn_f=add_to_version_number)
-    print(version_numbers)
+    print(wrap_execution(bits)[1])
 
 
 def part2(bits):
-    print(wrap_execution(bits))
+    print(wrap_execution(bits)[0])
 
 
 with open("input/day16.txt") as f:

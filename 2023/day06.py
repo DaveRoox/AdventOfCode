8a61d8e747f0903d23a723776bfaf435ba604bd2
DaveRoox <davexrusso@gmail.com>
2023-12-07 19:37:20 +0100

    Format file

--- 2023/day06.py
+++ 2023/day06.py
@@ -1,13 +1,16 @@
 from functools import reduce
 from math import ceil, floor
 
+
 def count_solutions(T, D):
     low = floor((T - (T*T - 4*D)**.5) / 2) + 1
     high = ceil((T + (T*T - 4*D)**.5) / 2) - 1
     return high - low + 1
 
+
 def part1(times_distances_pairs):
-    print(reduce(int.__mul__, map(lambda item: count_solutions(*item), times_distances_pairs)))
+    print(reduce(int.__mul__, map(
+        lambda item: count_solutions(*item), times_distances_pairs)))
 
 
 def part2(times_distances_pairs):
@@ -17,6 +20,7 @@ def part2(times_distances_pairs):
 
 
 with open("input/day06.txt") as f:
-    times_distances_pairs = [(t, d) for t, d in zip(*list(map(lambda l: list(map(int, l.replace('\n', '').split(': ')[1].strip().split())), f.readlines())))]
+    times_distances_pairs = [(t, d) for t, d in zip(
+        *list(map(lambda l: list(map(int, l.replace('\n', '').split(': ')[1].strip().split())), f.readlines())))]
     part1(times_distances_pairs)
     part2(times_distances_pairs)
